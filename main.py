e0656873f41a37e705881af614aee84ea6d9c091
nick <nordstromn@wit.edu>
2022-03-22 03:36:11 -0400

    finished normalizing how sites are extracted from url column. Process appears to work without error

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ main.py
 import data_loader
 
 INPUT_FILE = "Reddit_posts.csv"
+PROCESSED_FILE = "data.xlsx"
 
 
 def main():
-    df = data_loader.normalize_read_csv(INPUT_FILE)
+    # df = data_loader.normalize_read_csv(INPUT_FILE)
+    df = data_loader.read_excel(PROCESSED_FILE)
     print(df.columns)
     print(df)
 
